1a57305d39da60597c4ccf9e4b99eb6e89d4b1b5
Julio Cesar <juliocesardantss@gmail.com>
2023-06-28 11:13:05 -0300

    TDD implementado com as informacoes do enunciado

--- banco-xpto/banco-xpto.js
+++ banco-xpto/banco-xpto.js
@@ -19,9 +19,9 @@ class Conta {
     saque(valor) {
         let depositoAtual = this.saldo - valor
         return depositoAtual > this.saldo ?
-            `${this.cliente} nao tem saldo suficiente`
+            this.saldo = `${this.cliente} nao tem saldo suficiente`
             : depositoAtual < 0 ?
-                `${this.cliente} nao tem saldo suficiente`
+                this.saldo = `${this.cliente} nao tem saldo suficiente`
                 : this.saldo = depositoAtual
     }
     transferencia(ClienteDatranferencia, clienteRecebeTranferencia, valorDeTranferencia) {
@@ -60,11 +60,17 @@ class ContaCorrente extends Conta {
         }
     }
 }
+const joaquim = new ContaPoupanca("Joaquim", "poupanca", 1234, 333)
+
 const cadastrandoCliente = (cliente) => {
     const clientesCadastrados = []
     clientesCadastrados.push(cliente)
     return clientesCadastrados
 }
+joaquim.deposito(30)
+console.log(joaquim)
+joaquim.saque(40)
+console.log(joaquim)
 module.exports = {
     Cliente,
     Conta,
--- banco-xpto/banco-xpto.test.js
+++ banco-xpto/banco-xpto.test.js
@@ -1,8 +1,6 @@
 const banco = require("./banco-xpto")
-const Conta = banco.Conta
 const ContaCorrente = banco.ContaCorrente
 const ContaPoupanca = banco.ContaPoupanca
-const Cliente = banco.Cliente
 const cadastrandoCliente = banco.cadastrandoCliente
 
 test("Deve fazer um deposito, efetuar um saque e retornar o saldo", () => {
@@ -28,4 +26,25 @@ test("Deve fazer um saque, e retornar que o limite foi excedido", () => {
     expect(joaquim.limiteDeCredito).toBe("limite excedido")
 })
 
-
+test("Deve fazer um saque, e retornar o valor do saldo restante", () => {
+    const joaquim = new ContaPoupanca("Joaquim", "poupanca", 1234, 333)
+    cadastrandoCliente(joaquim)
+    joaquim.deposito(70)
+    joaquim.saque(30)
+    expect(joaquim.saldo).toBe(40)
+})
+test("Deve fazer um saque, e retornar o valor do saldo restante", () => {
+    const joaquim = new ContaPoupanca("Joaquim", "poupanca", 1234, 333)
+    cadastrandoCliente(joaquim)
+    joaquim.deposito(70)
+    joaquim.saque(30)
+    joaquim.deposito(400)
+    expect(joaquim.saldo).toBe(440)
+})
+test("Deve fazer um saque, e retornar que o cliente nao tem saldo suficiente", () => {
+    const joaquim = new ContaPoupanca("Joaquim", "poupanca", 1234, 333)
+    cadastrandoCliente(joaquim)
+    joaquim.deposito(30)
+    joaquim.saque(1031)
+    expect(joaquim.saldo).toBe(joaquim.cliente + " " + "nao tem saldo suficiente")
+})
